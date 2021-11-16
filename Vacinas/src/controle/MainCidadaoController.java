11d4a7e4eb56e7fca793fd301f47d1e178585b5d
Gabriel Gustavo Queiroz <gabrielgustavosantosqueiroz@gmail.com>
2021-11-15 21:10:55 -0300

    Att

---
+++ Vacinas/src/controle/MainCidadaoController.java
@@ -0,0 +1,93 @@
+package controle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
+public class MainCidadaoController {
+
+    @FXML
+    private Label agendado1;
+
+    @FXML
+    private Label agendado2;
+
+    @FXML
+    private Label agendado3;
+
+    @FXML
+    private Label agendado4;
+
+    @FXML
+    private Label agendado5;
+
+    @FXML
+    private CheckBox check1;
+
+    @FXML
+    private CheckBox check2;
+
+    @FXML
+    private CheckBox check3;
+
+    @FXML
+    private CheckBox check4;
+
+    @FXML
+    private CheckBox check5;
+
+    @FXML
+    private CheckBox check6;
+
+    @FXML
+    private CheckBox check7;
+
+    @FXML
+    private CheckBox check8;
+
+    @FXML
+    private CheckBox check9;
+
+    @FXML
+    private ChoiceBox<?> choiceBox;
+
+    @FXML
+    private Label covid1;
+
+    @FXML
+    private Label covid2;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private Label febreAfitosa;
+
+    @FXML
+    private Label febreAmarela;
+
+    @FXML
+    private Label hidrofobia;
+
+    @FXML
+    private Label paralisiaInfantil;
+
+    @FXML
+    private Label pesteBubonica;
+
+    @FXML
+    private Label tetano;
+
+    @FXML
+    private Label variola;
+
+    @FXML
+    void agendarButton(ActionEvent event) {
+
+    }
+
+}
+
